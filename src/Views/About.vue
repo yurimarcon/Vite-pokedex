d506853937442dfbf02eb5ae756382e0e500c48f
yurimarcon <yuri.erik.oliveira@gmail.com>
2022-03-05 17:22:55 -0300

    conclusao

--- src/Views/About.vue
+++ src/Views/About.vue
@@ -2,11 +2,12 @@
     <div class="container">
         <div class="card my-md-3 p-3 text-muted">
             <h1 class="">Sobre</h1>
+            <hr>
             <h2>Objetivo</h2>
             <p>Aqui irá uma descrição sobre o projeto, obrogado por acompanhar e bons estudos!!!</p>
             <p>Aqui irá uma descrição sobre o projeto, obrogado por acompanhar e bons estudos!!!</p>
             <p>Aqui irá uma descrição sobre o projeto, obrogado por acompanhar e bons estudos!!!</p>
-
+            <hr>
             <h2>Dúvidas</h2>
             <p>Em caso de dúvidas pode entrar em contato comigo através das mídias sociais.</p>
             <p>Em caso de dúvidas pode entrar em contato comigo através das mídias sociais.</p>
