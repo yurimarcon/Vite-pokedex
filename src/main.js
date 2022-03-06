c24eddfbbae99aec30801a80f428b934d517e2f9
yurimarcon <yuri.erik.oliveira@gmail.com>
2022-03-06 15:37:22 -0300

    pwa

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './Router'
 import './registerServiceWorker'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
+
 const app = createApp(App);
 app.use(router);
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import host from 'vite-plugin-host'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA()]
+  plugins: [
+    vue(), 
+    VitePWA({
+      workbox: {
+        cleanupOutdatedCaches: false  
+      }  
+    }), 
+    host()
+  ],
 })
