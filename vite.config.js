52d7c04a7ce8dfab4be7d143b7314a3da71d44e8
yurimarcon <yuri.erik.oliveira@gmail.com>
2022-03-07 16:34:27 -0300

    utmtst

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,13 @@ export default defineConfig({
       msTileColor: '#BDBDBD',
       appleMobileWebAppCapable: 'yes',
       appleMobileWebAppStatusBarStyle: '#DF0101',
-  
+      iconPaths: {
+        favicon32: 'img/icons/favicon-32x32.png',
+        favicon16: 'img/icons/favicon-16x16.png',
+        appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+        maskIcon: 'img/icons/safari-pinned-tab.svg',
+        msTileImage: 'img/icons/mstile-150x150.png'
+      },
       // configuração do workbox plugin
       workboxPluginMode: 'InjectManifest',
       workboxOptions: {
